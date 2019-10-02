1c924270eea0e2babe4a0bae832101125bd81fde
renatopcinfo <renatopcinfo@gmail.com>
2019-10-02 01:32:43 -0300

    express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-const express = require("express");
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(routes)
+
+app.listen(3333);
